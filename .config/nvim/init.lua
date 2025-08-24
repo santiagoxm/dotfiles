bd110f273ce670d22d528a0acc576b609647c29e
Santiago Moreno <santiagoxaviermoreno@gmail.com>
2025-08-24 16:53:19 -0300

    changed theme to unokai

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -50,7 +50,6 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 require("lazy").setup({
   spec = {
     -- add your plugins here
-    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
     {
     "folke/which-key.nvim",
     event = "VeryLazy",
@@ -72,27 +71,12 @@ require("lazy").setup({
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "catppuccin" } },
+  install = { colorscheme = { "unokai" } },
   -- automatically check for plugin updates
   checker = { enabled = false },
 })
 
-require("catppuccin").setup({
-    flavour = "mocha", -- latte, frappe, macchiato, mocha
-    transparent_background = true, -- disables setting the background color.
-    float = {
-        transparent = false, -- enable transparent floating windows
-        solid = false, -- use solid styling for floating windows, see |winborder|
-    },
-    show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
-    term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
-    dim_inactive = {
-        enabled = false, -- dims the background color of inactive window
-        shade = "dark",
-        percentage = 0.15, -- percentage of the shade to apply to the inactive window
-    },
-    auto_integrations = true,
-})
-
-vim.cmd.colorscheme "catppuccin"
-
+vim.cmd.colorscheme "unokai"
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })

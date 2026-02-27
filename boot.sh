fa9b2992e922dfe2fcf561330421dc2909682632
Santiago Moreno <santiagoxaviermoreno@gmail.com>
2026-02-27 01:49:35 -0300

    changed kernel options

--- boot.sh
+++ boot.sh
@@ -1,4 +1,4 @@
 sudo efibootmgr --create \
 --disk /dev/sda --part 1 --label "Arch Linux" \
 --loader /vmlinuz-linux \
---unicode 'root=PARTUUID=7a5179dd-bc49-438d-9b82-57fa0cc1896f ro loglevel=4 systemd.log_level=warning initrd=\amd-ucode.img'
+--unicode 'root=PARTUUID=7a5179dd-bc49-438d-9b82-57fa0cc1896f ro loglevel=4 systemd.show_status=auto rd.udev.log_level=4 initrd=\amd-ucode.img'
